c3a9f9087c77a639d42f305d2843edfe441de2fc
ilyapatis24 <ilyapatis24@mail.ru>
2024-08-21 21:08:28 +0300

    SOLID & DRY

---
+++ 04/02/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "Data.h"
+
+int main(int argc, char* argv[]) {
+    std::ofstream file("file.txt");
+    Printable* dataAsJSON = new DataAsJSON("data", Format::kJSON);
+    SaveToAsHTML saveToHTML;
+    saveToHTML.save(file, dataAsJSON);
+}
